b3f1c689dc4fa35a192d7f81166ab495848070fa
Maksim Ryzhikov <rv.maksim@gmail.com>
2016-08-21 02:26:51 +0300

    chore: made auxiliary procedures locals

--- lib/cbrt.scm
+++ lib/cbrt.scm
@@ -12,26 +12,20 @@
   (* x x))
 
 
-(define (improve guess x)
-  (/ (+ (/ x
-           (square guess))
-        (* guess 2))
-     3))
-
-
-(define (good-enough? guess x)
-  (< (abs (- guess
-             (improve guess x))) 0.001))
-
-
-(define (cbrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (cbrt-iter (improve guess x)
-                 x)))
-
-
 (define (cbrt x)
+  (define (cbrt-iter guess x)
+    (define (improve guess x)
+      (/ (+ (/ x
+               (square guess))
+            (* guess 2))
+         3))
+    (define (good-enough? guess x)
+      (< (abs (- guess
+                 (improve guess x))) 0.001))
+    (if (good-enough? guess x)
+        guess
+        (cbrt-iter (improve guess x)
+                   x)))
   (cbrt-iter 1.0 x))
 
 
